2f69f4c476992ddbda0c544840f180bee1793959
Samuel Sackey <smlscky@gmail.com>
2023-08-25 14:42:50 +0000

    Base ui setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,28 @@
 function App() {
   return (
-    <>
-      <div className="bg-gray-50 min-h-screen">Pokédex</div>
-    </>
+    <div>
+      <header className="flex h-16 items-center justify-between px-8 fixed top-0 left-1/2 transform -translate-x-1/2 w-full">
+        <h3 className="text-white">Pokédex</h3>
+
+        <div>
+          <input
+            type="search"
+            name="search"
+            placeholder="Search Pokémon"
+            className="py-3 px-5 rounded-lg text-sm focus:outline-none"
+          />
+        </div>
+      </header>
+
+      <section className="min-h-screen flex flex-col items-center">
+        <div className="container grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 gap-6 my-8">
+          <div className="bg-red-400">rsfrdf</div>
+          <div className="bg-red-400">rsfrdf</div>
+          <div className="bg-red-400">rsfrdf</div>
+          <div className="bg-red-400">rsfrdf</div>
+        </div>
+      </section>
+    </div>
   );
 }
 
