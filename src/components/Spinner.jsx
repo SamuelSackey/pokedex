21ed02306956310ed7e54d14b5212319468f60ba
Samuel Sackey <smlscky@gmail.com>
2023-08-26 17:02:21 +0000

    move asset

--- src/components/Spinner.jsx
+++ src/components/Spinner.jsx
@@ -1,11 +1,7 @@
 const Spinner = () => {
   return (
     <div className="animate-spin">
-      <img
-        src="/src/assets/pokeball.svg"
-        alt="loading pokeball"
-        className="w-36 h-36"
-      />
+      <img src="/pokeball.svg" alt="loading pokeball" className="w-36 h-36" />
     </div>
   );
 };
